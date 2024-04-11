5aef1c6a959e9775751dd07fe56d471c603e171e
Senyo197 <psenyo197@gmail.com>
2024-04-11 03:51:48 +0000

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { createUser, uploadPhoto } from './util';
+
+export default function handleProfileSignup() {
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  Promise.all([photoPromise, userPromise])
+    .then((value) => {
+      console.log(`${value[0].body}, ${value[1].firstName}, ${value[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
